56d703270bd2d81932bb4e302be34d84e9414f84
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2025-07-16 22:32:36 -0300

    feat: header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css';
+import logo from '../../assets/logo_teste.svg';
+import { FaWhatsapp } from 'react-icons/fa';
+
+export default function Header() {
+    return (
+      <header className={styles.header}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <nav className={styles.navBox}>
+          <a className={styles.navItem} href="">
+            Quem somos
+          </a>
+          <a className={styles.navItem} href="">
+            Equipe
+          </a>
+          <a className={styles.navItem} href="">
+            Áreas de Atuação
+          </a>
+          <a className={styles.navItem} href="">
+            Publicações
+          </a>
+        </nav>
+        <button className={styles.contactButton}>
+          <FaWhatsapp size={18}/>
+          Fale conosco
+        </button>
+      </header>
+    );
+}
