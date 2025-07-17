373af8b4b11c03e1c6aab40b1ceb204ea14433f1
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2025-07-17 20:43:22 -0300

    refactor: componente nossos diferenciais

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,91 +1,13 @@
 import HomeBanner from "../../components/HomeBanner";
 import HomeQuemSomos from "../../components/HomeQuemSomos";
-import styles from "./styles.module.css";
-import { PiHandshakeLight } from "react-icons/pi";
+import NossosDiferenciais from "../../components/NossosDiferenciais";
 
 export default function Home() {
   return (
     <>
       <HomeBanner />
       <HomeQuemSomos />
-      <section>
-        <div className={styles.container}>
-          <div className={styles.backgroundImage}></div>
-          <div className={styles.contentBox}>
-            <h2 className={styles.differentialsTitle}>
-              Nossos<span> Diferenciais</span>
-            </h2>
-            <div className={styles.differentials}>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-              <div className={styles.differential}>
-                <div className={styles.iconContainer}>
-                  <h3>Flexibilidade</h3>
-                  <PiHandshakeLight color="#DB9F5D" size={36} />
-                </div>
-                <p className={styles.differentialText}>
-                  Nossa equipe é flexível na tratativa com o cliente. Você é
-                  chamado pelo nome, entendemos a sua realidade e momento.
-                  Independentemente do certame que você almeja.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <NossosDiferenciais />
     </>
   );
 }
