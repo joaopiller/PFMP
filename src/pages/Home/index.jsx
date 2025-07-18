ce45edc2b1132dcbb3140ccc74566bb8d7da5b81
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2025-07-17 22:05:01 -0300

    feat: nossa equipe

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import HomeBanner from "../../components/HomeBanner";
 import HomeQuemSomos from "../../components/HomeQuemSomos";
 import NossosDiferenciais from "../../components/NossosDiferenciais";
+import member from "../../assets/member.jpg";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
@@ -8,6 +10,31 @@ export default function Home() {
       <HomeBanner />
       <HomeQuemSomos />
       <NossosDiferenciais />
+      <section className={styles.equipeSection}>
+        <div className={styles.equipeContainer}>
+          <h2 className={styles.equipeTitle}>
+            Nossa <span>Equipe</span>
+          </h2>
+          <div className={styles.equipeMembers}>
+            <div className={styles.equipeMember}>
+              <img src={member} alt="Membro da equipe" />
+              <p>Victor Hugo Piller Menezes</p>
+            </div>
+            <div className={styles.equipeMember}>
+              <img src={member} alt="Membro da equipe" />
+              <p>Victor Hugo Piller Menezes</p>
+            </div>
+            <div className={styles.equipeMember}>
+              <img src={member} alt="Membro da equipe" />
+              <p>Victor Hugo Piller Menezes</p>
+            </div>
+            <div className={styles.equipeMember}>
+              <img src={member} alt="Membro da equipe" />
+              <p>Victor Hugo Piller Menezes</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
