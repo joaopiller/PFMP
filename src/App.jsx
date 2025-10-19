0ba6a7e457e54f4a802d82887d0008ecbd638b33
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2025-10-19 20:31:21 -0300

    fix: site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 export default function App() {
   return (
-    <React.StrictMode>
       <RouterProvider router={router} />
-    </React.StrictMode>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { ViteSSG } from "vite-ssg";
-import App from "./App.jsx";
-import "./index.css";
-
-export const createApp = ViteSSG(App);
+import { StrictMode } from 'react' 
+import { createRoot } from 'react-dom/client' 
+import './index.css' 
+import App from './App.jsx' 
+createRoot(document.getElementById('root')).render( <StrictMode> <App /> </StrictMode>, )
