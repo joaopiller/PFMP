607c1cc6163c57f637082b9c660270a903d4c51d
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2025-11-02 18:50:28 -0300

    try to fix

--- src/data/equipeMembers.js
+++ src/data/equipeMembers.js
@@ -1,7 +1,7 @@
-import victorImg from "../assets/images/victor_h_piller.JPG";
-import pedroImg from "../assets/images/pedro_fonseca.JPG";
-import flaviaImg from "../assets/images/flavia_perim.JPG";
-import marchesiImg from "../assets/images/joao_marchesi.JPG";
+import victorImg from "../assets/images/victor_h_piller.jpg";
+import pedroImg from "../assets/images/pedro_fonseca.jpg";
+import flaviaImg from "../assets/images/flavia_perim.jpg";
+import marchesiImg from "../assets/images/joao_marchesi.jpg";
 
 const equipeMembers = [
     {
