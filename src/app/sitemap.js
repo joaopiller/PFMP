6efa739861d39d9e353029aec2eb5dbab9cfdbb9
João Vicente Piller <joaovicentepm14@gmail.com>
2025-12-09 09:50:18 -0300

    fix: sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,7 @@
 import posts from "@/data/posts";
 
 export default function sitemap() {
-  const baseUrl = "http://pfmpadv.com.br";
+  const baseUrl = "https://pfmpadv.com.br";
   const now = new Date().toISOString();
 
   const staticPages = [
@@ -12,12 +12,16 @@ export default function sitemap() {
     "publicacoes",
   ];
 
-  const staticUrls = staticPages.map((path) => ({
-    url: `${baseUrl}/${path}`,
-    lastModified: now,
-    changeFrequency: "monthly",
-    priority: path === "" ? 1.0 : 0.9,
-  }));
+  const staticUrls = staticPages.map((path) => {
+    const url = path === "" ? baseUrl : `${baseUrl}/${path}`;
+    
+    return {
+      url: url,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: path === "" ? 1.0 : 0.9,
+    };
+  });
 
   const postUrls = posts.map((post) => ({
     url: `${baseUrl}/publicacoes/${post.id}`,
